5b3a187b831a1ed641cee60d6979ab19a153e207
isaac-rand <63428408+isaac-rand@users.noreply.github.com>
2020-06-11 16:55:56 -0400

    Commented Code + Efficiency Update
    Added comments all throughout code. Also, changed assignment of team logos to also depend on alphabetical order. This should be more efficient if it's working how I think it's working

--- Sister-Teams-Explorer/server.R
+++ Sister-Teams-Explorer/server.R
@@ -7,18 +7,30 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(sf)
-library(leaflet)
-library(dplyr)
+#Loading the libraries the app is dependent on
+library(shiny)      #server - ui interaction, drawing
+library(sf)         #spatial data handling
+library(leaflet)    #creating interactive maps
+library(dplyr)      #data handling
 
+#reading kbodata shapefile
 kbodata <- st_read("data/kbodata.shp") 
+
+#sorting kbodata alphabetically
+#this ends up being pretty important, only change this if you're ready to change a bunch else
+#(mostly for assigning rankedTeams() and icon values to the right team)
 kbodata <- arrange(kbodata, team_nm)
+
+#defining points at centroids of cities teams are in
 kbodata$point = st_centroid(kbodata$geometry)
+
+#spreading out the three teams from Seoul so that they do not occupy the same point
 kbodata[kbodata$team_nm == "LG Twins", ]$point <- kbodata[kbodata$team_nm == "LG Twins", ]$point + c(-0.1, 0.5)
 kbodata[kbodata$team_nm == "Doosan Bears", ]$point <- kbodata[kbodata$team_nm == "Doosan Bears", ]$point + c(0.25, 0.25)
 kbodata[kbodata$team_nm == "Kiwoom Heroes", ]$point <- kbodata[kbodata$team_nm == "Kiwoom Heroes", ]$point + c(0.4, 0)
 
+#This defines the links shown in the map popups. The first link is to the team wiki, the second is to the team's city's wiki.
+#html is used to turn the links from strings into real links
 link <- c("<a href = https://en.wikipedia.org/wiki/Doosan_Bears> Learn More about the Doosan Bears</a><br>
           <a href = https://en.wikipedia.org/wiki/Seoul> Learn More about Seoul</a>",
            "<a href = https://en.wikipedia.org/wiki/Hanwha_Eagles> Learn More about the Hanwha Eagles</a><br>
@@ -39,20 +51,31 @@ link <- c("<a href = https://en.wikipedia.org/wiki/Doosan_Bears> Learn More abou
           <a href = https://en.wikipedia.org/wiki/Daegu> Learn More about Daegu</a>",
            "<a href = https://en.wikipedia.org/wiki/SK_Wyverns> Learn More about the SK Wyverns</a><br>
           <a href = https://en.wikipedia.org/wiki/Incheon> Learn More about Incheon</a>")
+#links were laid out in alphabetical order, matching the data's order so a simple join works like this
 kbodata$link <- link
+
+#reading in the data on mlb teams/cities
 mlbdata <- st_read("data/mlbdata.shp")
 
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw app
 shinyServer(function(input, output) {
     
+    #creates a list of the mlb names
     mlbNames <- as.list(mlbdata$team_nm)
     
+    #defines input which allows users to select their mlb team from that list
+    #selects Arizona Diamondbacks (alphabetically first) as the default
     output$teamSelect <- renderUI({
         selectInput("mlbteam", "Pick Your Favorite MLB Team:", (mlbNames), selected = "Arizona Diamondbacks") 
     })
     
-
+    #defines the data frame which is eventually output beneath the input drop downs
+    #includes team name, home city, and calculated Difference from MLB team for all KBO Teams for a given comparison factor
+        # first checks what the comparison factor is
+        # then binds into a data frame the team, the city, and the difference (calculated in line)
+        # then renames all columns
+        # then sorts rows by how different KBO teams are from MLB teams
     rankedTeams <- reactive({
         if(input$comp == "Number of Championships Won"){
             cbind.data.frame(kbodata$team_nm, 
@@ -99,6 +122,8 @@ shinyServer(function(input, output) {
                 arrange(Difference)
         }
         else{
+            #difference for "All the Above" defined here
+            #difference = sum of difference for all other comparison criteria
             cbind.data.frame(kbodata$team_nm, 
                              kbodata$city_nm, 
                              abs(mlbdata[mlbdata$team_nm == input$mlbteam,]$pop_std - kbodata$pop_std) +
@@ -113,48 +138,58 @@ shinyServer(function(input, output) {
         
     })
 
-
+    #defines the actual table to be drawn using the rankedTeams() dataframe generated above
+    #Set caption "Sister Teams" to be drawn above table
     output$table <- renderTable({rankedTeams()}, caption=paste("Sister Teams"),
                                 caption.placement = getOption("xtable.caption.placement", "top"))
+    
+    #defines the icons to be displayed on the map for each time
+    #sets both the image and the size
     leafIcons <- reactive({
          icons(
-                iconUrl = ifelse(kbodata$team_nm == "Doosan Bears",
-                                 "imgs/doosan_bears.png",
-                            ifelse(kbodata$team_nm == "NC Dinos", 
-                                   "imgs/nc_dinos.png", 
-                            ifelse(kbodata$team_nm == "Hanwha Eagles", 
-                                   "imgs/hanwha_eagles.png",
-                            ifelse(kbodata$team_nm == "Samsung Lions", 
-                                   "imgs/samsung_lions.png",
-                            ifelse(kbodata$team_nm == "Kiwoom Heroes",
-                                   "imgs/kiwoom_heroes.png",
-                            ifelse(kbodata$team_nm == "SK Wyverns",
-                                   "imgs/sk_wyverns.png",
-                            ifelse(kbodata$team_nm == "LG Twins",
-                                   "imgs/lg_twins.png",
-                            ifelse(kbodata$team_nm == "KT Wiz",
-                                   "imgs/kt_wiz.png",
-                            ifelse(kbodata$team_nm == "Kia Tigers",
-                                   "imgs/kia_tigers.png",
-                                   "imgs/lotte_giants.png"))))))))),
+                #sets the right image for the right team depending on team name
+                #the icons are set in alphabetical order (the order of the data set as imposed above)
+                #since they are in alphabetical order here, they are each assigned to the right team
+                #this saves a long switch or many nested ifelse statements
+                iconUrl = (c("imgs/doosan_bears.png", 
+                            "imgs/hanwha_eagles.png",
+                            "imgs/kia_tigers.png",
+                            "imgs/kiwoom_heroes.png",
+                            "imgs/kt_wiz.png",
+                            "imgs/lg_twins.png",
+                            "imgs/lotte_giants.png",
+                            "imgs/nc_dinos.png", 
+                            "imgs/samsung_lions.png",
+                            "imgs/sk_wyverns.png")),
+                
+               
+                #sets the icon dimensions to 45 - 15 * the standardized Difference
+                #standardized difference = how much more or less different this team is from the MLB team compared to the others
+                #standardizing the difference ends up being important because one team, is unfortunately unparalleled.
+                #The New York Yankees have just one too many championships, their difference scores end up really high
+                #all teams differences end up standardized basically to avoid this
+                #but there is an added bonus! without standardizing all the greater differences would end up making the icons smaller when 'all the above' is selected without it
+                #once again, the right team is assigned the right data because of the consistent order of the teams in the data
                 iconWidth = 45 - 15 * (((arrange(rankedTeams(), Team)$Difference) - mean(rankedTeams()$Difference)) / sd(rankedTeams()$Difference)),
                 iconHeight = 45 - 15 * (((arrange(rankedTeams(), Team)$Difference) - mean(rankedTeams()$Difference)) / sd(rankedTeams()$Difference))
                 )
     })
     
-    #iconWidth = 10 * filter(rankedTeams(), KBOTeamName == kbodata$team_nm)$Difference, 
-    #iconHeight = 10 * filter(rankedTeams(), KBOTeamName == kbodata$team_nm)$Difference
+    #defines the leaflet output map
     leafMap <- reactive({
         leaflet() %>%
+            #adds basemap from Stamen
             addProviderTiles(providers$Stamen.TonerLite,
                              options = providerTileOptions(noWrap = TRUE)
             ) %>%
+            #adds the icons defined above to the map at the point stored in kbodata
             addMarkers(data = kbodata$point, 
                        icon = leafIcons(),
+                       #sets up popup. If you click on the icon in the app. it will allow you to navigate to team or city wikis.
                        popup = kbodata$link)
         })
     
+    #defines the actual map to be drawn in UI as the map above. Names it map.
     output$map <- renderLeaflet({leafMap()})
     
-    output$disclaimer <- renderText("DISCLAIMER: All of the comparisons above are provied for entertainment purposes only. You should root for the NC Dinos regardless of the results. #StrongerTogether")
 })
--- Sister-Teams-Explorer/ui.R
+++ Sister-Teams-Explorer/ui.R
@@ -14,36 +14,66 @@ library(leaflet)
 
 
 
-# Define UI for application that draws a histogram
+# Define UI for the application
 shinyUI(fluidPage(
 
     # Application title
     titlePanel("KBO Sister Teams Explorer for MLB Fans"),
 
-    # Sidebar with a slider input for number of bins
+    # This defines the information displayed on the side bar (computer) or at the top of the page (mobile)
     sidebarLayout(
-        sidebarPanel(
-            uiOutput("teamSelect"),
-            selectInput(inputId = "comp",
+        sidebarPanel( 
+            
+            # this creates/draws the MLB team dropdown, defined in server
+            uiOutput("teamSelect"),   
+            
+            # this defines and creates the comparison criteria selector
+            selectInput(#sets ID for access in server
+                        inputId = "comp", 
+                        #sets display title
                         label = "Choose a Criteria for Comparison:",
+                        #sets choices in dropdown menu
                         choices = c("Number of Championships Won",
                                     "Age of Team",
                                     "Home City GDP",
                                     "Home City Average Annual Temperature",
                                     "Home City Population",
                                     "All of the Above"),
+                        #sets defaut selection to NUmber of Championships Won
                         selected = "Number of Championships Won"
             ),
+            #this creates/draws the table output of the results defined in the server
             tableOutput("table")
             
         ),
 
     
-        # Show a plot of the generated distribution
+        # This defines the output on the right (computer) or bottom (mobile)
         mainPanel(
+            #this draws the map output defined in the server
             leafletOutput("map"),
+            
+            #this puts a new line between the map output and the text below
             br(),
-            textOutput("disclaimer")
+            
+            #this draws HTML formatted text.
+            #If you are familiar with html, wrapping something in tags$x is equivalent to wrapping it in <x> </x>
+            #If you are unfamiliar -- h4 is the tag for smaller header, p is for body text, and a is for links
+            #Empty p tags are included for new lines, br() is supposed to do this, but it was not working
+            #Check here for a full tag glossary if you're interested https://shiny.rstudio.com/articles/tag-glossary.html
+            tags$div(class="header", checked=NA,
+                     tags$h4("What does all of this mean?"),
+                     tags$p("Every comparison metric used here is internally standardized within the leagues, for more meaningful results. This standardization converts the original data about the teams/cities from both leagues to a new scale, where 0 is mean for the league, and all values represent how many standard deviations the original data was from the mean for the league."),
+                     tags$a(href = "https://www.statisticshowto.com/standardized-variables/#:~:text=In%20statistics%2C%20standardized%20variables%20are,were%20measured%20on%20different%20scales", "For more on standardization, click here"),
+                     tags$p(""),
+                     tags$p("The app as it stands allows users to select one MLB team and one comparison criteria at a time and see what KBO teams are most similar to it using that criteria. The results are output as a table and as a map. The table shows all the KBO teams, their home city, and a field called `Difference`. `Difference` is the absolute value of the difference between the standardized value of the comparison criteria for the MLB team/city selected and the standardized value of the comparison criteria for each KBO team/city. The map output shows each team represented by a point which is shown by its team logo, and sized in inverse proportion to the team's `Difference`."),
+                     tags$strong("If that didn't make any sense, what's key is that bigger logos = more similar, smaller logos = more different! And KBO teams are listed from most to least similar on the left!"),
+                     tags$p(""),
+                     tags$p("When \"All the Above\" is chosen as the comparison criteria, the `Difference` field is equal to the sum of the `Difference` field  as calculated for all of the other comparison criteria."),
+                     tags$a(href = "https://github.com/isaac-rand/sister-teams-explorer", "Check out the project github for more information on data sources and methods :)"),
+                     tags$p(""),
+                     tags$p("DISCLAIMER: All of the comparisons above are provied for entertainment purposes only. You should root for the NC Dinos regardless of the results. #StrongerTogether")
+            )
         )
     )
 ))
